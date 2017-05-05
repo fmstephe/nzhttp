c8e1c083f6b7c33a54d1c6a3491a397d3238bb3b
dlapets <lapets@adjust.com>
2017-05-05 14:34:43 +0200

    HistoGRAM GOD DAMN

--- main.go
+++ main.go
@@ -6,12 +6,17 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"sync"
 	"time"
+
+	"github.com/codahale/hdrhistogram"
 )
 
 var (
-	server = flag.Bool("s", false, "If set runs in server mode. Otherwise runs in runClient mode.")
-	port   = flag.String("p", "9123", "Sets the port to use")
+	server   = flag.Bool("s", false, "If set runs in server mode. Otherwise runs in runClient mode.")
+	port     = flag.String("p", "9123", "Sets the port to use")
+	requests = flag.Int("n", 1000, "Number of requests sent by each sender in client mode")
+	senders  = flag.Int("m", 100, "Number of senders to use in client mode")
 )
 
 func main() {
@@ -23,26 +28,59 @@ func main() {
 	}
 }
 
-func runClient() {
-	for i := 0; i < 100; i++ {
-		go send()
-	}
-	time.Sleep(time.Hour)
+func runServer() {
+	fmt.Printf("Server starting on port %s\n", *port)
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
-func send() {
-	for {
+func runClient() {
+	numSenders := *senders
+
+	fmt.Printf("Client about to send %d requests * %d senders\n", *requests, numSenders)
+	histChan := make(chan (*hdrhistogram.Histogram), numSenders)
+	wg := &sync.WaitGroup{}
+	for i := 0; i < numSenders; i++ {
+		wg.Add(1)
+		go send(wg, histChan)
+	}
+	wg.Wait()
+	close(histChan)
+
+	// combine results from workers into one histogram
+	hist := newHistogram()
+	var dropped int64
+	for curHist := range histChan {
+		dropped += hist.Merge(curHist)
+	}
+
+	// print some results
+	fmt.Printf("Merging dropped %d samples\n", dropped)
+	for _, f := range []float64{1, 25, 50, 75, 90, 95, 99, 100} {
+		v := hist.ValueAtQuantile(f)
+		fmt.Printf("%3.0f: %8dms\n", f, v/int64(time.Millisecond))
+	}
+
+}
+
+func send(wg *sync.WaitGroup, ch chan *hdrhistogram.Histogram) {
+	hist := newHistogram()
+	for i := 0; i < *requests; i++ {
+		before := time.Now()
 		resp, err := http.Get("http://localhost:" + *port)
+		hist.RecordValue(int64(time.Since(before)))
+
 		if err != nil {
 			log.Fatal(err)
 		}
 		resp.Body.Close()
 	}
+	ch <- hist
+	wg.Done()
 }
 
-func runServer() {
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+func newHistogram() *hdrhistogram.Histogram {
+	return hdrhistogram.New(0, int64(time.Minute), 5)
 }
